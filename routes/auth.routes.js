6ca02f5028d61950a7a8e95e9fe0a93fea82539d
Fras0 <frasasker0@gmail.com>
2023-04-18 00:23:31 +0200

    adding demo home page

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@ const epxress = require("express");
 const authController = require("../controllers/auth.controller");
 const router = epxress.Router();
 
+router.get("/", authController.getAllProducts);
 router.get("/signup", authController.getSignUp);
 router.get("/login", authController.getLogIn);
 
